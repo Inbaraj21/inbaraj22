7ebc5ba86dc53e87d4dd7b56a74ff7398a4c16f4
Inbaraj21 <111624912+Inbaraj21@users.noreply.github.com>
2022-08-22 19:47:36 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,35 @@
+ class triangle
+
+{
+ void add()
+  {
+   for(int row=1;row<=4;row++)
+    {
+     
+      for(int space=3;space>=row;space--)
+       {
+        System.out.print(" ");
+        }
+        for(int star=1;star<=row;star++)
+         {
+          System.out.print("*");
+           
+        }
+      for(int col=1;col<row;col++)
+        {
+          System.out.print("*"); 
+          }
+         System.out.println();
+       }
+  }
+
+ public static void main(String args[])
+  {
+    triangle t=new triangle();
+     t.add();
+   }
+}
+
+
+
+
